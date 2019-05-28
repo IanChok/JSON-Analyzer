2d21d71e2441fcc84f5ba3789ae784b4bcff5f1f
IanChok <ichok87@outlook.com>
2019-05-28 02:04:34 -0700

    updated test file

--- test/parserTest.js
+++ test/parserTest.js
@@ -1,5 +1,3 @@
-/* test/sum.js */
-
 const server = require('../server');
 const parser = require('../parser');
 const expect = require('chai').expect;
@@ -33,6 +31,7 @@ describe('Querying Data', () => {
     it('should return a json object', async () => {
       expect(transactionData).to.be.an('object');
     })
+
   })
 
 
@@ -48,22 +47,30 @@ describe('Querying Data', () => {
 
   describe('Returning Filtered Data', () => {
     describe('Querying AND Requests', () => {
+      let reqNonExist = '{and: ["non_exist"]}';
       let reqStatus = '{and: ["status"]}';
       let reqStatusAndLosers = '{and:["status", "losers"]}';
-      let reqFirstName = '{and:["first_name"]}'
-      let reqFirstNameAndLastName = '{and: ["first_name", "last_name"]}'
+      let reqFirstName = '{and:["first_name"]}';
+      let reqFirstNameAndLastName = '{and: ["first_name", "last_name"]}';
+      let reqQuizThenSportThenQ1ThenQuestion = '{and: ["quiz", {and: ["sport", {and: ["q1", {and: "question"}]}]}]}';
+      let reqStatusAnd_WinnersThenName_LosersThenName = '{and: ["status", "winners", {and: ["name"]}, "losers", {and: ["name"]}]}'
 
-      it('reqStatus should return "status" field  for transactionData', () => {
+      
+      it('reqNonExist should return null because "non_exist" field does not exist from transactionData', ()=> {
+        expect(parser(reqNonExist, transactionData)).to.be.null;
+      })
+
+      it('reqStatus should return "status" field  from transactionData', () => {
         expect(parser(reqStatus, transactionData)).to.eql([{
           status: "SUCCESS"
         }]);
       })
 
-      it('reqStatusAndLosers should return "status" and "losers" field for transactionData', () => {
-        expect(parser(reqStatusAndLosers)).to.eql([{
+      it('reqStatusAndLosers should return "status" and "losers" field from transactionData', () => {
+        expect(parser(reqStatusAndLosers, transactionData)).to.eql([{
           status: "SUCCESS",
           losers: [{
-            "name": "noobmaster69",
+            "name": "noobmaster",
             "country": "CA",
             "amountLoss": "80.25",
             "currency": "EUR"
@@ -71,29 +78,104 @@ describe('Querying Data', () => {
         }])
       })
 
-      it('reqFirstName should return all "first_name" fields of each object in the array of peopleData', () => {
-        expect(parser(reqFirstName)).to.eql([{
+      it('reqFirstName should return all "first_name" fields of each object in the array from peopleData', () => {
+        expect(parser(reqFirstName, peopleData)).to.eql([{
           first_name: ['Jeanette', 'Giavani', 'Noell', 'Willard']
         }])
       })
 
-      it('reqFirstName and LastName should return all "first_name" and "last_name" fields of each object in the array of peopleData', () => {
-        expect(parser(reqFirstNameAndLastName)).to.eql([
+      it('reqFirstName and LastName should return all "first_name" and "last_name" fields of each object in the array from peopleData', () => {
+        expect(parser(reqFirstNameAndLastName, peopleData)).to.eql([
           { first_name: "Jeanette", last_name: "Penddreth" },
           { first_name: "Giavani", last_name: "Frediani" },
           { first_name: "Noell", last_name: "Bea"},
           { first_name: "Willard", last_name: "Valek"}
         ])
       })
-    })
+      
+      it('reqQuizThenSportThenQ1ThenQuestion should return nested properties: Quiz -> Sport -> Q1 -> Question from quizData', () => {
+      expect(parser(reqQuizThenSportThenQ1ThenQuestion, quizData)).to.eql([{
+        quiz: [{
+          sport: [{
+            q1: [{
+              question: "Which one is correct team name in NBA?"
+            }]
+          }]
+        }]
+      }])
+      })
+
+      it('reqStatusAnd_WinnersThenName_LosersThenName should return multiple fields with their associated nested contents from transactionData', () => {
+        expect(parser(reqStatusAnd_WinnersThenName_LosersThenName, transactionData)).to.eql([{
+          status: "SUCCESS",
+          winners: [{name: ["null", "Test2Ailbhe", "omegazhenga", "noobmaster"]}],
+          losers:[{name: ["noobmaster"]}]
+        }])
+      })
+
+      })
+
 
     describe('Querying OR Requests', () => {
-      let reqStatusOrLosers = '{or:[\'status\', \'losers\']}';
+      let reqNonExistOrStatus = '{or: ["non_exist", "status"]}';
+      let reqStatusOrLosers = '{or: ["status", "losers"]}';
 
-      it('reqStatusORLosers should return "status" field for transactionData', () => {
-        expect(parser(reqStatusOrLosers)).to.eql({
+      it('reqStatusORLosers should return "status" field from transactionData', () => {
+        expect(parser(reqStatusOrLosers, transactionData)).to.eql({
           status: "SUCCESS"
         });
+      })
+
+      it('reqNonExistOrStatus should return "status" field because "non_exist" field doesn\'t exist from transactionData', ()=> {
+        expect(parser(reqNonExistOrStatus, transactionData)).to.eql([{
+          status: "SUCCESS"
+        }])
+      })
+
+      it('reqNonExistOrStatus')
+
+    })
+
+    describe('Querying DEEP requests', ()=> {
+      let reqQuestion = '{deep: [{and: ["question"]}]}'
+      let reqQuestionWithParents = '{deep: [{and: ["question"]}], parents: true}';
+      let reqQuestionAndAnswer = '{deep: [{and: ["question", "answer"]}]}'
+
+      it('reqQuestion should return all "question" fields, regardless of their nested levels from quizData', () => {
+        expect(parser(reqQuestion, quizData)).to.eql([{
+          question: [
+            "Which one is correct team name in NBA?",
+            "5 + 7 = ?",
+            "12 - 8 = ?"]}
+        ])
+      })
+
+      it('reqQuestion should return all "question" fields with nested structure intact, regardless of their nested levels from quizData', () => {
+        expect(parser(reqQuestionWithParents, quizData)).to.eql([{
+          quiz: [{
+            sport: [{
+              q1: {
+                question: "Which one is correct team name in NBA?"
+              }
+            }],
+            maths: [{
+              q1: [{
+                question: "5 + 7 = ?"
+              }],
+              q2: [{
+                question: "12 - 8 = ?"
+              }]
+            }]
+          }]
+        }])
+      })
+
+      it('reqQuestion should return all "question" and "answer" fields, regardless of their nested levels from quizData', () => {
+        expect(parser(reqQuestionAndAnswer, quizData)).to.eql([
+          { question: "Which one is correct team name in NBA?", answer: "Huston Rocket" },
+          { question: "5 + 7 = ?", answer:  "12" },
+          { question: "12 - 8 = ?", answer: "4"}
+        ])
       })
 
     })
